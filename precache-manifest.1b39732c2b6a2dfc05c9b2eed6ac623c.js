23899fc18a257c85ed5a96559463b569fa6be1aa
Divyansh <divyansh.rana12@gmail.com>
2020-09-21 03:26:09 +0530

    Updates

---
+++ precache-manifest.1b39732c2b6a2dfc05c9b2eed6ac623c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe3a9635349f23160c0becc156d13e5",
+    "url": "/swiflearnassign/index.html"
+  },
+  {
+    "revision": "19b5233ab1c3b3573bf5",
+    "url": "/swiflearnassign/static/css/main.4ec64df1.chunk.css"
+  },
+  {
+    "revision": "dc2d3f7723ed0417e7c9",
+    "url": "/swiflearnassign/static/js/2.03cc669b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swiflearnassign/static/js/2.03cc669b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b5233ab1c3b3573bf5",
+    "url": "/swiflearnassign/static/js/main.f36d1866.chunk.js"
+  },
+  {
+    "revision": "33ec0424d82b78115c11",
+    "url": "/swiflearnassign/static/js/runtime-main.fe70e6c6.js"
+  },
+  {
+    "revision": "6544b1015d3315a7888f80b5ac1208d7",
+    "url": "/swiflearnassign/static/media/live.6544b101.ico"
+  },
+  {
+    "revision": "4aac3d235682cc9a0cb05c4682c55eb4",
+    "url": "/swiflearnassign/static/media/youtube.4aac3d23.webp"
+  }
+]);
